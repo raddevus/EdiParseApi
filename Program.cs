16fd4fb2e58b0dc1d59b4a5670e58535673dd8b4
raddevus <1radeutsch1@gmail.com>
2025-03-30 17:49:18 -0400

    Moved the call to build app down futher - after that occurs you cannot add any more services so it needs to be last.

--- Program.cs
+++ Program.cs
@@ -6,8 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
-
 // Add services to the container
 builder.Services.AddControllers();
 
@@ -15,7 +13,7 @@ builder.Services.AddDbContext<EdiContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IEdiParser, EdiParser>();
 builder.Services.AddScoped<IEdiRepository, EdiRepository>();
-
+var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
